f96fa14b0948ee220cfbfb4bc7f4560eb8924bb0
webpg11month <webprogrammermash@gmail.com>
2021-01-30 19:32:31 +0900

    edit_manshou

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    ZebraAppTest
+                    ZebraAppmanshou
                 </div>
 
                 <div class="links">
