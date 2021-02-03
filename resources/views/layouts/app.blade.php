7fcacdf7d4d6efbacd329d56e60146d2cd096957
webpg11month <webprogrammermash@gmail.com>
2021-02-03 22:11:15 +0900

    header completed layout fix edit_manshou

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,20 +15,98 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
+    <link rel="stylesheet" href="https://cdn.rawgit.com/filipelinhares/ress/master/dist/ress.min.css">
+    <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
+    <header>
+        <div class="mobil-setting"><!-- モバイル設定 -->
+          <nav class="flex-search">
+              <div class="hamburger">
+                <input type="checkbox" id="hamburger-check" class="chekc-hidden">
+                <label for="hamburger-check" class="hamburger-open">
+                  <span></span>
+                </label>
+                <label for="hamburger-check" class="hamburger-close"></label>
+                <nav class="hamburger-content">
+                    <ul class="hamburger-list">
+                      <li class="hamburger-item">
+                          <a href="#">ハードウェア</a>
+                      </li>
+                      <li class="hamburger-item-1">
+                          <a id="hamburger-link1" href="#">ニュース</a>
+                      </li>
+                      <li class="hamburger-item-1">
+                          <a id="hamburger-link2" href="#">ランキング</a>
+                      </li>
+                      <li class="hamburger-item-1">
+                        <a id="hamburger-link3" href="#">セール情報</a>
+                      </li>
+                      <li class="hamburger-item">
+                          <a id="hamburger-link4" href="#">ソフトウェア</a>
+                      </li>
+                      <li class="hamburger-item-1">
+                        <a id="hamburger-link5" href="#">ニュース</a>
+                      </li>
+                      <li class="hamburger-item-1">
+                        <a id="hamburger-link6" href="#">人気言語</a>
+                      </li>
+                      <div class="hamburger-item-footer">
+                        <li class="hamburger-item-2">
+                          <a id="hamburger-link7" href="#">問い合わせ</a>
+                        </li>
+                        @guest
+                            <li class="hamburger-item-2">
+                                <a id="hamburger-link8" href="{{ route('login') }}">{{ __('ログイン') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="hamburger-item-2">
+                                    <a id="hamburger-link9" href="{{ route('register') }}">{{ __('サインアップ') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="hamburger-item-2">
+                                <a href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                    {{ __('ログアウト') }}
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </li>
+                        @endguest
+                      </div>
+                    </ul>
+                </nav>
+              </div>
+              <!-- <img id="fadeInOutImg" src="search.png" alt="検索"> -->
+              <!-- <p id="fadeInOutImg">〇</p></div> -->
+              <div class="switch-btn">
+                <i class="fa fa-search size" id="fadeInOutImg" aria-hidden="true"></i>
+              </div>
+              <h1 class="header-search-1">Zebra</h1>
+              <div class="header-fadeImg">
+              <form class="header-fadeImg-1" action="sample01.html" method="get">
+                  <input type="text" name="search" value="" placeholder="キーワードを入力">
+                  <button>検索</button>
+                </form>
+              </div>
+          </nav>
+          <script type="text/javascript" src="js/common.js"></script>
+        </div><!-- モバイル設定  完了 -->
+      </header>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Zebra') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
@@ -41,17 +119,15 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
-                            <a class="dropdown-item">
-                                {{ Auth::user()->first_name }} <span class="caret"></span>
-                            </a>
+
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                 onclick="event.preventDefault();
                                                 document.getElementById('logout-form').submit();">
@@ -70,5 +146,11 @@
             @yield('content')
         </main>
     </div>
+    @guest
+    @else
+    <a class="dropdown-item">
+        {{ Auth::user()->first_name }} <span class="caret"></span>
+    </a>
+    @endguest
 </body>
 </html>
