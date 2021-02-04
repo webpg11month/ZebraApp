7cc881f749714fa4b7a73a4f329a909d015ed5fd
webpg11month <webprogrammermash@gmail.com>
2021-02-04 19:41:40 +0900

    footer add edit_manshou

---
+++ public/js/common.js
@@ -0,0 +1,24 @@
+'use strict';
+
+$(function(){
+    $(function() {
+      $(".switch-btn").on('click', function() {
+      $(".header-fadeImg").fadeToggle("fast",alertFunc);
+    });
+    function alertFunc(){
+        if ($(this).css('display') === 'block') {
+          $(".switch-btn").html("<i class='fa fa-times size' id='fadeInOutImg' aria-hidden='false'></i>");
+        }else{
+          $(".switch-btn").html("<i class='fa fa-search size' id='fadeInOutImg' aria-hidden='true'></i>");
+        }
+      };
+    });
+  });
+  $('.hamburger-open').click(function() {
+    //属性の削除
+    $("#fadeInOutImg").remove();
+  })
+  $('.hamburger-close').click(function() {
+    //属性の削除
+    $(".switch-btn").html("<i class='fa fa-search size' id='fadeInOutImg' aria-hidden='true'></i>");
+  })
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<footer>
+    <div class="footer-icon-flex">
+      <i class="fab fa-twitter-square size"></i>
+      <i class="fab fa-facebook-square size"></i>
+      <i class="fab fa-instagram size"></i>
+      <i class="fab fa-github-square size"></i>
+    </div>
+    <small>&copy; 2021 株式会社zebra</small>
+  </footer>
