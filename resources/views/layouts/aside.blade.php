e30d369e212862a4898ba849e9efa5520b2e2b05
Yasuhito Nishide <yasuhitonishide@gmail.com>
2021-02-04 22:11:43 +0900

    サーチコンソール追加
    edit_nishide

--- resources/views/layouts/aside.blade.php
+++ resources/views/layouts/aside.blade.php
@@ -1,56 +1,76 @@
     <!-- サイドバー始まり -->
     <div class="sub-all">
-    <aside id="sub">
-        <div class="side-heder">
-            <h1 class="header-logo">Zebra</h1>
-        </div>
-        <h2 class="side-heading">ハードウェア</h2>
-        <ul class="side-frame">
-            <li>ニュース</li>
-            <li>ランキング</li>
-            <li>セール情報</li>
-        </ul>
-        <h2 class="side-heading">ソフトウェア</h2>
-        <ul class="side-frame">
-            <li>ニュース</li>
-            <li>人気言語</li>
-        </ul>
-        <div class="side-foot">
-            <ul id="side-footer">
-                <li class="side-contact">問い合わせ</li>
-                <div class="login">
-                    @guest
+        <aside id="sub">
+            <div class="side-heder">
+                <h1 class="header-logo">Zebra</h1>
+            </div>
+            <h2 class="side-heading">ハードウェア</h2>
+            <ul class="side-frame">
+                <li>ニュース</li>
+                <li>ランキング</li>
+                <li>セール情報</li>
+            </ul>
+            <h2 class="side-heading">ソフトウェア</h2>
+            <ul class="side-frame">
+                <li>ニュース</li>
+                <li>人気言語</li>
+            </ul>
+            <div class="side-foot">
+                <ul id="side-footer">
+                    <li class="side-contact">問い合わせ</li>
+                    <div class="login">
+                        @guest
 
                         <li><a id="sideA" href="{{ route('login') }}">{{ __('ログイン') }}</a></li>
                         @if (Route::has('register'))
                         <li><a id="sideA" href="{{ route('register') }}">{{ __('サインアップ') }}</a></li>
 
-                    @endif
-                    @else
-                    <a class="logout" href="{{ route('logout') }}" onclick="event.preventDefault();
+                        @endif
+                        @else
+                        <a class="logout" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                 document.getElementById('logout-form').submit();">
-                        <li>
-                            {{ __('ログアウト') }}</li>
-                    </a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        @csrf
-                    </form>
-                    @endguest
-                </div>
-            </ul>
+                            <li>
+                                {{ __('ログアウト') }}</li>
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                        @endguest
+                    </div>
+                </ul>
+            </div>
+            <div class="small">
+                <small>(c)ZebraApp <br>
+                    mash & nishide</small>
+            </div>
+        </aside>
+        <!-- サイドバー終わり -->
+        <!-- サーチコンソール -->
+        <div class="search-wrapper">
+            <div class="input-holder">
+                <form action="#" method="GET">
+                <input name="search" type="text" class="search-input" value=""/>
+                <button class="search-icon" onclick="searchToggle(this, event);"><i class="fas fa-search"></i></button>
+                </form>
+            </div>
+            <span class="close" onclick="searchToggle(this, event);"></span>
         </div>
-        <div class="small">
-            <small>(c)ZebraApp <br>
-                mash & nishide</small>
-        </div>
-    </aside>
-    <!-- サイドバー終わり -->
-    <!-- サーチコンソール -->
-    <div id="search">
-        <input type="text" name="q" size="23" maxlength="200" value="サイト内検索" id="keyword"
-            onfocus="search_window_flag(1);" onblur="search_window_flag(0);" onkeydown="enter();">
-        <img src="search_button.gif" alt="検索" onclick="search_submit();" id="search_btn">
+        <!-- サーチコンソール -->
     </div>
-    <!-- サーチコンソール -->
-    </div>
-
+    {{-- サーチコンソール --}}
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script>
+        function searchToggle(obj, evt){
+    var container = $(obj).closest('.search-wrapper');
+        if(!container.hasClass('active')){
+            container.addClass('active');
+            evt.preventDefault();
+        }
+        else if(container.hasClass('active') && $(obj).closest('.input-holder').length == 0){
+            container.removeClass('active');
+            // clear input
+            container.find('.search-input').val('');
+        }
+}
+    </script>
+{{-- サーチコンソール --}}
