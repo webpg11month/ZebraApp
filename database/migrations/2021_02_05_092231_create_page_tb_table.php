29484cddcc4f455565a54737afbd2a3488895194
Yasuhito Nishide <yasuhitonishide@gmail.com>
2021-02-05 18:38:22 +0900

    page_tbをmigrateしたときにエラーが出ました edit_nishide

---
+++ database/migrations/2021_02_05_092231_create_page_tb_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePageTbTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('page_tb', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->int('page_id')->unique();
+            $table->string('text',255);
+            $table->string('images',255);
+            $table->enum('flg',['1','2','3']);
+            $table->string('etc',255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('page_tb');
+    }
+}
