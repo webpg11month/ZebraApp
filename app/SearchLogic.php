01d6a494fe54cd25d995b4e74d06e572af9d2687
webpg11month <webprogrammermash@gmail.com>
2021-02-09 20:40:43 +0900

    debug fixed edit_manshou

---
+++ app/SearchLogic.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Page;
+
+class SearchLogic extends Model
+{
+    public function search($page_data){
+        if($page_data){
+            //検索値選択
+            $keyword = $page_data['search'];
+        }else{
+            //全選択
+            $keyword = "";
+        }
+        //page_tb取得
+        $query = Page::query();
+        //空の場合でない場合は、検索される
+        if(!empty($keyword)){
+         $query->where('text','like','%'.$keyword.'%');
+        }
+        $pages = $query->orderBy('id','asc')->paginate(7);
+
+        return $pages;
+    }
+}
