62986d713d6030e29afbb03d6caa3dcaea1cab95
Yasuhito Nishide <yasuhitonishide@gmail.com>
2021-02-11 15:15:50 +0900

    edit_nishide

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -9,13 +9,13 @@
       <input class="lname" type="text" name="last-name" placeholder="名" value="">
     </div>
     <div class="contact-mail">
-    <input class="contact-mail-form" type="text" name="mail" value="" placeholder="メールアドレス">
+    <input class="contact-mail-form" type="text" name="email" value="" placeholder="メールアドレス">
     <input class="contact-remail-form" type="text" name="remail" value="" placeholder="メールアドレス確認用">
     </div>
     <span class="item-name">お問い合わせ内容</span>
 
     <div class="textarea-wrap">
-      <textarea name="name" rows="8" cols="80" value=""></textarea>
+      <textarea name="text" rows="8" cols="80" value=""></textarea>
     </div>
     </div>
     <div class="submit-wrap">
